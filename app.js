ca50086d1bd58e325d32e9d277740410a6680f51
sara-hincapieh <123768467+sara-hincapieh@users.noreply.github.com>
2023-06-02 14:03:54 -0500

    s

--- app.js
+++ app.js
@@ -1,45 +1,42 @@
-const cuadrado= document.querySelectorAll(".cuadrado");
+const cuadrado = document.querySelectorAll(".cuadrado");
 const tiempofaltante = document.querySelector("#tiempo");
-let puntaje = document.getElementsById ("puntaje")
+let puntaje = document.getElementsById("puntaje");
 
 //
 
-let resultado= 0;
-let tiempoactual= tiempofaltante.textContent
+let resultado = 0;
+let tiempoactual = tiempofaltante.textContent;
 
-
-function cuadradoazar {
-  cuadrado.forEach (nombredeclase => {
-    nombredeclase.classList.remove ("topo");
-  })
-  let posicionalazar = cuadrado [Math.floor(Math.random() *9)]
+function cuadradoazar() {
+  cuadrado.forEach((nombredeclase) => {
+    nombredeclase.classList.remove("topo");
+  });
+  let posicionalazar = cuadrado[Math.floor(Math.random() * 9)];
   posicionalazar.classList.add("topo");
-  posiciontopo.posicionalazar.Id
+
+  posiciontopo = posicionalazar.id;
 }
 
-
-
-
-cuadrado.forEach(identificador =>{
-  identificador.addEventListener("click"()=> {
-if (identificador.Id === posiciontopo){
-  resultado = resultado +1;
-  puntaje.textContent = resultado;
-  posiciontopo= null;
-}
+cuadrado.forEach((identificador) => {
+  identificador.addEventListener("click", () => {
+    if (identificador.id === posiciontopo) {
+      resultado = resultado + 1;
+      puntaje.textContent = resultado;
+      posicionalazar = null;
+    }
   });
-  });
+});
 
-  function movertopo (){
-    tiempotopo = setInterval (cuadradoazar,700)
-  }
-  movertopo();
-  function cuentaregresiva (){
-tiempoactual--;
-tiempofaltante.textContent = teimpoactual;
-if (tiempoactual==0){
-clearInterval (idtiempo);
-clearInterval (idtiempotopo);
-alert("se acabo el tiempo tu puntaje fue" +resultado+ "topos atrapados")
+function movertopo() {
+  tiempotopo = setInterval(cuadradoazar, 700);
 }
+movertopo();
+function cuentaregresiva() {
+  tiempoactual--;
+  tiempofaltante.textContent = tiempoactual;
+  if (tiempoactual == 0) {
+    clearInterval(idtiempo);
+    clearInterval(idtiempotopo);
+    alert("se acabo el tiempo tu puntaje fue" + resultado + "topos atrapados");
   }
+}
